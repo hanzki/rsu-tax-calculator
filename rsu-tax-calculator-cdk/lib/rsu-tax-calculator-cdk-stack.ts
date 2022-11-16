e9035bac16ae9dec5409ef352277ede1cb0b5bd2
Hannu Huhtanen <hannu.huhtanen@gmail.com>
2022-11-16 21:52:24 +0200

    Added automatic deployment to AWS

---
+++ rsu-tax-calculator-cdk/lib/rsu-tax-calculator-cdk-stack.ts
@@ -0,0 +1,26 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { StaticSite } from './static-site';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class RsuTaxCalculatorCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'RsuTaxCalculatorCdkQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+
+    new StaticSite(this, 'StaticSite', {
+      domainName: 'huhtanen.eu',
+      siteSubDomain: 'tax-calculator',
+      ecbDomain: 'sdw-wsrest.ecb.europa.eu',
+      ecbPath:'/service/data/EXR/D.USD.EUR.SP00.A',
+      ecbParams:['startPeriod', 'format', 'detail'],
+      sourcePath: this.node.tryGetContext('sourcePath')
+    });
+  }
+}
---
+++ rsu-tax-calculator-cdk/test/rsu-tax-calculator-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RsuTaxCalculatorCdk from '../lib/rsu-tax-calculator-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/rsu-tax-calculator-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RsuTaxCalculatorCdk.RsuTaxCalculatorCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
